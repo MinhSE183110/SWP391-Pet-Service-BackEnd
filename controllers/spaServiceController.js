ce27303cd1f9cd1e5312ee3897629f2daf3278db
MinhthnSE183110 <minhthnse183110@fpt.edu.vn>
2024-06-23 22:41:10 +0700

    add new

---
+++ controllers/spaServiceController.js
@@ -0,0 +1,105 @@
+const SpaService = require('../models/SpaService');
+const cloudinary = require('../config/cloudinary');
+const { generateServiceID } = require('../utils/utils');
+// Create a service
+exports.createService = async (req, res) => {
+  try {
+    const serviceID = await idGenerators.generateServiceID();
+    const { ServiceName, Description, PetTypeID, Price, Status } = req.body;
+    let newService = new SpaService({
+      ServiceID: serviceID,
+      ServiceName: ServiceName,
+      Description: Description,
+      PetTypeID: PetTypeID,
+      Price: Price,
+      Status: Status,
+    });
+
+    if (req.file && req.file.path) {
+      const result = await cloudinary.uploader.upload(req.file.path); // Upload image to Cloudinary
+      newService.ImageURL = result.secure_url; // Save the URL from Cloudinary
+    }
+
+    await newService.save();
+    res.status(201).json(newService);
+  } catch (error) {
+    console.error('Error creating service:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// Get all services
+exports.getAllServices = async (req, res) => {
+  try {
+    const services = await SpaService.find();
+    res.status(200).json(services);
+  } catch (error) {
+    console.error('Error fetching services:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// Get a service by ID
+exports.getServiceById = async (req, res) => {
+  try {
+    const service = await SpaService.findOne({ ServiceID: req.params.id });
+    if (!service) return res.status(404).json({ message: 'Service not found' });
+    res.status(200).json(service);
+  } catch (error) {
+    console.error('Error fetching service:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// Update a service
+exports.updateService = async (req, res) => {
+  const { id } = req.params;
+  const updateData = req.body;
+  delete updateData.serviceId;
+  try {
+    if (req.file && req.file.path) {
+      const service = await SpaService.findOne({ ServiceID: id });
+
+      if (!service) {
+        return res.status(404).json({ message: 'Service not found' });
+      }
+
+      if (service.ImageURL) {
+        const publicId = service.ImageURL.split('/').pop().split('.')[0];
+        await cloudinary.uploader.destroy(publicId);
+      }
+
+      const result = await cloudinary.uploader.upload(req.file.path);
+      updateData.ImageURL = result.secure_url;
+    }
+
+    const service = await SpaService.findOneAndUpdate(
+      { ServiceID: id },
+      updateData,
+      { new: true }
+    );
+
+    if (!service) {
+      return res.status(404).json({ message: 'Service not found' });
+    }
+
+    res.status(200).json({ message: 'Service updated successfully', service });
+  } catch (error) {
+    console.error('Error updating service:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// Delete a service
+exports.deleteService = async (req, res) => {
+  try {
+    const service = await SpaService.findOneAndDelete({ ServiceID: req.params.id });
+    if (!service) return res.status(404).json({ message: 'Service not found' });
+
+    res.status(200).json({ message: 'Service deleted successfully' }); // Thêm status 200 để báo thành công
+  } catch (error) {
+    console.error('Error deleting service:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
