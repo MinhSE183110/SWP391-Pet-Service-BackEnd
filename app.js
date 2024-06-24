9891cfc19e795a4bc73ea7d3605fb46ba2ab9306
MinhSE183110 <166373840+MinhSE183110@users.noreply.github.com>
2024-06-24 15:05:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -27,13 +27,7 @@ const app = express();
 connectDB();
 
 // Middleware
-var corsOptions = {
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'PATCH'], // Add all methods you want to allow
-  allowedHeaders: '*', // Allow all headers
-  credentials: true // Enable credentials
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
